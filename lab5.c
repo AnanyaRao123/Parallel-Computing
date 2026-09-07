a10a3bc529d24de5a935029e136d326cab698fe4
Ananya <169143294+AnanyaRao123@users.noreply.github.com>
2026-09-07 11:45:02 +0530

    MPI Program to demonstration of MPI_Send and MPI_Recv

--- lab5.c
+++ lab5.c
@@ -7,7 +7,7 @@ int main(int argc,char *argv[]){
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&size);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    if(siize<2){
+    if(size<2){
         if(rank==0){
             printf("This program requires atleast 2 processors.\n");
         }
